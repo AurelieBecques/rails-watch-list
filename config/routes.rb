0735ca66c3bc6dd5eed33a9c1012905d2fde5792
Aurelie Becques <aurelie.becques@gmail.com>
2022-11-17 19:28:01 +0100

    watch-list

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+    resources :reviews, only: :create
+  end
+  resources :bookmarks, only: [:destroy]
+  resources :reviews, only: [:destroy]
 end

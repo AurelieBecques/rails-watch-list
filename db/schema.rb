eb0bae94dbae099c48259a354c8757f0ee065216
Aurelie Becques <aurelie.becques@gmail.com>
2022-11-18 08:24:43 +0100

    watch-list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_115620) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_18_071347) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_115620) do
     t.text "overview"
     t.float "rating"
     t.text "poster_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
